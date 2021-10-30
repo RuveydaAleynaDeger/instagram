b586f6d3ca0c67aea8fcd86437825d0356998298
RuveydaAleynaDeger <degerece29@gmail.com>
2021-10-30 14:01:56 +0300

    Camera sisteme entegre edildi.

--- App.js
+++ App.js
@@ -29,7 +29,7 @@ import RegisterScreen from "./components/auth/Register";
 import LoginScreen from './components/auth/Login'
 import MainScreen from './components/Main'
 import AddScreen from './components/main/Add'
-
+import SaveScreen from './components/main/Save'
 import { createStackNavigator } from "@react-navigation/stack";
 import { TextInput } from "react-native-gesture-handler";
 const Stack = createStackNavigator();
@@ -84,11 +84,11 @@ export class App extends Component {
     return (
       <Provider store={store}>
         <NavigationContainer >
-          <Stack.Navigator initialRouteName="Main">
+        <Stack.Navigator initialRouteName="Main">
             <Stack.Screen name="Main" component={MainScreen} />
             <Stack.Screen name="Add" component={AddScreen} navigation={this.props.navigation}/>
-            {/* <Stack.Screen name="Save" component={SaveScreen} navigation={this.props.navigation}/>
-            <Stack.Screen name="Comment" component={CommentScreen} navigation={this.props.navigation}/> */}
+            <Stack.Screen name="Save" component={SaveScreen} />
+    
           </Stack.Navigator>
         </NavigationContainer>
       </Provider>
